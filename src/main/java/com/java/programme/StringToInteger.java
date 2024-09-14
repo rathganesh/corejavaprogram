9ab98f1af866412e6d8d8d6feecc3990c9c32cdd
Ganesh Rath <rathganesh1992@gmail.com>
2024-09-14 10:31:24 +0530

    Added new java programme

---
+++ src/main/java/com/java/programme/StringToInteger.java
@@ -0,0 +1,35 @@
+package com.java.programme;
+
+public class StringToInteger {
+    private int negativeNumber = 0;
+    private int output = 0;
+    private int number =0;
+
+    public void convertStringToInteger(String Input){
+        try {
+            System.out.println("String "+Input+" convert to number : "+Integer.parseInt(Input));
+        }catch (NumberFormatException exception){
+            throw new NumberFormatException("String "+Input+" format is not correct");
+        }
+    }
+
+    public void convertStringToIntegerWithOutPredefinedMethod(String Input){
+        if (Input.charAt(0)=='-'){
+            negativeNumber = 1;
+        }
+        for (int i = this.negativeNumber; i<Input.length(); i++){
+           if (!Character.isDigit(Input.charAt(i))){
+               throw new NumberFormatException("String "+Input+" format is not correct");
+           }
+            number = Input.charAt(i) - '0';
+            output = number + output*10;
+        }
+        if (negativeNumber==1){
+             System.out.println("String "+Input+" convert to number : "+-output);
+        }else
+            System.out.println("String "+Input+" convert to number : "+output);
+        negativeNumber = 0;
+        output = 0;
+        number =0;
+    }
+}
--- src/main/java/com/testcae/executor/Runner.java
+++ src/main/java/com/testcae/executor/Runner.java
@@ -1,20 +1,23 @@
 package com.testcae.executor;
 
-import com.java.programme.CharaterCountInAString;
-import com.java.programme.DuplicateCharacterCount;
-import com.java.programme.LeapYear;
-import com.java.programme.VowelChecker;
+import com.java.programme.*;
 
 public class Runner {
     public static void main(String[] args){
-        CharaterCountInAString charaterCountInAString = new CharaterCountInAString();
-        charaterCountInAString.characterCount("ganesh kumar rath");
+        /*CharaterCountInAString charaterCountInAString = new CharaterCountInAString();
+        charaterCountInAString.characterCount("ganesh kumar rath");*/
         /*LeapYear leapYear = new LeapYear();
         leapYear.validateLeapYearOrNot(2024);*/
         /*VowelChecker vowelChecker = new VowelChecker();
         vowelChecker.validateVowelCheck('a');*/
         /*DuplicateCharacterCount duplicateCharacterCount = new DuplicateCharacterCount();
         duplicateCharacterCount.duplicateCharCount("ganesh kumar rath");*/
+        /*StringToInteger stringToInteger = new StringToInteger();
+        stringToInteger.convertStringToInteger("50");
+        stringToInteger.convertStringToInteger("50a");
+        stringToInteger.convertStringToIntegerWithOutPredefinedMethod("589");
+        stringToInteger.convertStringToIntegerWithOutPredefinedMethod("-589");
+        stringToInteger.convertStringToIntegerWithOutPredefinedMethod("-a589");*/
     }
 
 }
